9cf4e60c7f77ab3a46529bc7b18e79cd220fe1c1
Hannah Scheufele <hannah@cameoDevs-Mac-mini.local>
2017-04-04 15:21:59 +0200

    added individual light component functions

--- main.ts
+++ main.ts
@@ -2,20 +2,20 @@ enum Sensitivity {
     //% blockIdentity=calliopeBunt.sensitivity
     //% block="16496 Lux - 40ms"
     S16 = 0b000,
-    //% blockIdentity=calliopeBunt.sensitivity
-    //% block="8248 Lux - 80ms"
+        //% blockIdentity=calliopeBunt.sensitivity
+        //% block="8248 Lux - 80ms"
     S8 = 0b001,
-    //% blockIdentity=calliopeBunt.sensitivity
-    //% block="4124 Lux - 160ms"
+        //% blockIdentity=calliopeBunt.sensitivity
+        //% block="4124 Lux - 160ms"
     S4 = 0b010,
-    //% blockIdentity=calliopeBunt.sensitivity
-    //% block="2062 Lux - 320ms"
+        //% blockIdentity=calliopeBunt.sensitivity
+        //% block="2062 Lux - 320ms"
     S2 = 0b011,
-    //% blockIdentity=calliopeBunt.sensitivity
-    //% block="1031 Lux - 640ms"
+        //% blockIdentity=calliopeBunt.sensitivity
+        //% block="1031 Lux - 640ms"
     S1 = 0b100,
-    //% blockIdentity=calliopeBunt.sensitivity
-    //% block="515 Lux - 1280ms"
+        //% blockIdentity=calliopeBunt.sensitivity
+        //% block="515 Lux - 1280ms"
     S0 = 0b101,
 }
 
@@ -41,7 +41,7 @@ namespace sensors {
     //% blockId=calliopeBunt_sensitivity block="set light sensitivity to %sensitivity=sensitivity_id"
     //% parts="calliope-bunt"
     //% trackArgs=0
-    export function calliopeBuntSetSensitivity(sensitivity: number): void{
+    export function calliopeBuntSetSensitivity(sensitivity: number): void {
         colorSensitivity = sensitivity > Sensitivity.S0 ? Sensitivity.S0 : sensitivity;
     }
 
@@ -86,5 +86,73 @@ namespace sensors {
         return basic.rgbw(red, green, blue, white);
     }
 
+    /**
+     * Get RGBW values from Calliope mini RGB sensor.
+     */
+    //% blockId=calliopeBunt_green block="erkannte Grün Farbe"
+    //% parts="calliope-bunt"
+    //% trackArgs=0
+    export function calliopeBuntGreen(): number {
+        // green
+        pins.i2cWriteNumber(ADDRESS, 0x09, NumberFormat.Int8LE, true);
+        let green = pins.map(pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE), 0, 65535, 0, 255);
+        // -- enable for debugging
+        // serial.writeString("G[");
+        // serial.writeNumber(green);
+        // serial.writeString("]\r\n");
+        return green;
+
+    }
+
+    /**
+     * Get RGBW values from Calliope mini RGB sensor.
+     */
+    //% blockId=calliopeBunt_red block="erkannte Rot Farbe"
+    //% parts="calliope-bunt"
+    //% trackArgs=0
+    export function calliopeBuntRed(): number {
+        // red
+        pins.i2cWriteNumber(ADDRESS, 0x08, NumberFormat.Int8LE, true);
+        let red = pins.map(pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE), 0, 65535, 0, 255);
+        // -- enable for debugging
+        // serial.writeString("R[");
+        // serial.writeNumber(red);
+        // serial.writeString("]\r\n");
+        return red;
+    }
+
+    /**
+     * Get RGBW values from Calliope mini RGB sensor.
+     */
+    //% blockId=calliopeBunt_blue block="erkannte Blau Farbe"
+    //% parts="calliope-bunt"
+    //% trackArgs=0
+    export function calliopeBuntBlue(): number {
+        // blue
+        pins.i2cWriteNumber(ADDRESS, 0x0A, NumberFormat.Int8LE, true);
+        let blue = pins.map(pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE), 0, 65535, 0, 255);
+        // -- enable for debugging
+        // serial.writeString("B[");
+        // serial.writeNumber(blue);
+        // serial.writeString("]\r\n");
+        return blue;
+    }
+
+    /**
+     * Get RGBW values from Calliope mini RGB sensor.
+     */
+    //% blockId=calliopeBunt_white block="erkannte Helligkeit"
+    //% parts="calliope-bunt"
+    //% trackArgs=0
+    export function calliopeBuntWhite(): number {
+        // white
+        pins.i2cWriteNumber(ADDRESS, 0x0B, NumberFormat.Int8LE, true);
+        let white = pins.map(pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE), 0, 65535, 0, 255);
+        // -- enable for debugging
+        // serial.writeString("W[");
+        // serial.writeNumber(white);
+        // serial.writeString("]\r\n");
+        return white;
+    }
 
 }
